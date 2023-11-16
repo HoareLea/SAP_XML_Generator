3b17a98fd118bc66ef66885a7b31ede0a230d102
Ryan de Mello <ryan.demello@hoarelea.com>
2023-11-16 14:26:24 +0000

    Initial code review

--- generate.py
+++ generate.py
@@ -1,39 +1,50 @@
+"""Holds Class for generating the output"""
+
 from tkinter.filedialog import askopenfile, askdirectory
-from functions import *
+import warnings
 import pandas as pd
+from functions import input_reader, match_xml, data_to_xml, prettify, find_and_replace, ErrorFound
+
+warnings.simplefilter(action='ignore', category=UserWarning)
 
 class SAP:
+    """The main Class for handling the inputs and outputs"""
     def __init__(self, sheet, name, output_path):
         self.sheet = sheet
         self.name = name
-        self.compiler = self.compiler()
-    
-    def compiler(self):
-        print('Generating: ', self.name)
         self.input_unit = input_reader(self.sheet)
         self.output_data = match_xml(self.input_unit)
+        self.compiler(output_path)
+
+    def compiler(self, output_path):
+        """Begins the writing process if there is data to outputs"""
+        print('Generating: ', self.name)
         if self.output_data:
-            self.writer = self.writer()
+            self.writer(output_path)
         else:
-            raise Exception('No output data')
-    
-    def writer(self):
+            raise ErrorFound('No output data')
+
+    def writer(self, output_path):
+        """Writes out the xml data to a file"""
         xml_string = data_to_xml(self.output_data)
-        file_path = output_path+'\{}.xml'.format(self.name)
-        with open(file_path,'w') as f:
+        file_path = output_path+f'\{self.name}.xml'
+        with open(file_path,'w', encoding="utf-8") as f:
             try:
                 f.write(prettify(xml_string))
-                findAndReplace(file_path)
-            except:
-                raise Exception('Invalid XML structure')
+                find_and_replace(file_path)
+            except Exception as exc:
+                raise ErrorFound('Invalid XML structure') from exc
         pass
 
 if __name__ == "__main__":
-    excel_path = askopenfile(title='Select SAP Calc Sheet', filetypes=[("Excel files", ".xlsx .xls")])
-    output_path = askdirectory()
+    excel_path = askopenfile(
+        title='Select SAP Calc Sheet',
+        filetypes=[("Excel files", ".xlsx .xls")]
+    )
+    this_output_path = askdirectory()
     print('SAP Calc Sheet: '+excel_path.name)
     df = pd.read_excel(excel_path.name, header=1, sheet_name=None)
 
-    for name, sheet in df.items():
-        if 'Unit' in name:
-            sap = SAP(sheet,name,output_path)
+    for this_name, this_sheet in df.items():
+        if 'Unit' in this_name:
+            sap = SAP(this_sheet,this_name,this_output_path)
